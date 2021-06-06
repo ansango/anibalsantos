b166b0c8ce4fce514cf4c3221440e442fa12d297
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-06 18:55:58 +0200

    repost

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -6,64 +6,57 @@ import { BlogSeo } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 
-const postDateTemplate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+const postDateTemplate = { year: 'numeric', month: 'short', day: 'numeric' }
 
 export default function PostLayout({ children, frontMatter, next, prev }) {
   const { date, title, tags, readingTime, author } = frontMatter
   const min = Math.round(readingTime.minutes)
-  const toRead = `- ${min} ${min !== 1 ? 'minutos de lectura' : 'minuto de lectura'}`
+  const toRead = `${min} ${min !== 1 ? 'minutos leyendo' : 'minuto de leyendo'}`
   return (
     <SectionContainer>
       <BlogSeo url={`${siteMetadata.siteUrl}/blog/${frontMatter.slug}`} {...frontMatter} />
       <article>
         <div className="">
           <header className="pt-6 xl:pb-6">
-            <div className="space-y-1 text-center">
+            <div className="space-y-1">
               <div>
                 <PageTitle>{title}</PageTitle>
               </div>
             </div>
           </header>
           <div className="pb-8">
-            <dl className="pt-6 pb-10">
-              <dt className="sr-only">Authors</dt>
-              <dd>
-                <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
-                  <li className="flex items-center space-x-2">
-                    <img src={siteMetadata.image} alt="avatar" className="w-10 h-10 rounded-full" />
-                    <dl className="text-sm font-medium leading-5 whitespace-nowrap">
+            <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-8">
+              <div className="flex items-center">
+                <dl>
+                  <dt className="sr-only">Autor</dt>
+                  <dd className="flex items-center">
+                    <img
+                      src={siteMetadata.image}
+                      alt="avatar"
+                      className="w-6 h-6 rounded-full mr-3"
+                    />
+                    <dl className="text-sm text-gray-700 dark:text-gray-300 mr-3">
                       <dt className="sr-only">Name</dt>
-                      <dd className="text-gray-900 dark:text-gray-100">{siteMetadata.author}</dd>
-                      {typeof siteMetadata.twitter === 'string' && (
-                        <>
-                          <dt className="sr-only">Instagram</dt>
-                          <dd>
-                            <Link
-                              href={siteMetadata.instagram}
-                              target="_blank"
-                              className="text-yellow-500 hover:text-yellow-600 dark:hover:text-yellow-400"
-                            >
-                              {siteMetadata.instagram.replace('https://instagram.com/', '@')}
-                            </Link>
-                          </dd>
-                        </>
-                      )}
+                      <dd>{`${siteMetadata.author}`}</dd>
                     </dl>
-                  </li>
-                </ul>
-              </dd>
-            </dl>
-            <dl className="space-y-10">
-              <div>
-                <dt className="sr-only">Publicado</dt>
-                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                  <time dateTime={date}>
-                    {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}{' '}
-                  </time>
-                  <span>{toRead}</span>
-                </dd>
+                  </dd>
+                </dl>
+                <dl className="text-sm text-gray-700 dark:text-gray-300">
+                  <dt className="sr-only">Publicado</dt>
+                  <dd>
+                    <time dateTime={date} className="truncate">
+                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                    </time>
+                  </dd>
+                </dl>
               </div>
-            </dl>
+              <div className="flex items-center">
+                <dl className="py-4">
+                  <dt className="sr-only">Lectura</dt>
+                  <dd className="text-sm text-gray-500 min-w-32">{toRead}</dd>
+                </dl>
+              </div>
+            </div>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
               <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">{children}</div>
             </div>
