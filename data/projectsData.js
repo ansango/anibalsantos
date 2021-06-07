49fc4e839766a88a32a42aef541ed0d707c07687
ansango <anibalsantosgo@gmail.com>
2021-06-07 14:48:10 +0200

    proyectos

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,42 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What is you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Curriculum',
+    description: `Mi Curriculum Vitae desarrollado en una sola página con ES6, Sass, y HTML. Utilizando una estructura de maquetación OOCSS e ITCSS para mejorar mantenimiento`,
+    imgSrc: '/static/images/profile.png',
+    href: 'https://anibalsantos-resume.netlify.app/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'NgBrew',
+    description:
+      'Pequeña aplicación desarrollada en Angular 12 y TailwindCSS, con implementación de Service Workers y PWA, instalable y accesible offline',
+    imgSrc: '/static/images/brew.jpg',
+    href: 'https://ng-brew.netlify.app/',
+  },
+  {
+    title: 'Undefined Band',
+    description: 'Landing de producto, creada con Parcel, React, Bootstrap 5 y Sass, from scratch',
+    imgSrc: '/static/images/band.jpg',
+    href: 'https://band-uoc.netlify.app/',
+  },
+  {
+    title: 'Neo Punk Api',
+    description:
+      'Pequeña api rest pública realizada en Express y Mongo DB, consumida por el proyecto NgBrew',
+    imgSrc: '/static/images/brew.jpg',
+    href: 'https://neopunkapi.herokuapp.com/',
+  },
+  {
+    title: 'Shopping List',
+    description:
+      'Lista de la compra, instalable con PWA y LocalStorage, desarrollada con Nuxt.js, Vuex y TailwindCSS',
+    imgSrc: '/static/images/',
+    href: 'https://nuxt-shopping-list.netlify.app/',
+  },
+  {
+    title: '',
+    description: '',
+    imgSrc: '/static/images/',
+    href: '',
   },
 ]
 
