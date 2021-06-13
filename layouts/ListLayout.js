67128684848cea8643139339a3f965b24405f99f
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-13 14:43:12 +0200

    blog fix

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -62,7 +62,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
             const { slug, date, title, summary, tags } = frontMatter
             return (
               <li key={slug}>
-                <article className="xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline xl:mb-4">
+                <article className="py-4 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline xl:py-0">
                   <dl>
                     <dt className="sr-only">Published on</dt>
                     <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
@@ -71,21 +71,21 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                       </time>
                     </dd>
                   </dl>
-                  <div className="space-y-3 xl:col-span-3">
+                  <div className="xl:col-span-3">
                     <div>
-                      <h3 className="text-2xl mb-3 font-bold leading-8 tracking-tight">
+                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
                         <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
                           {title}
                         </Link>
                       </h3>
-                      <div className="flex flex-wrap">
-                        {tags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
+                      <div className="prose text-gray-500 max-w-none dark:text-gray-400">
+                        {summary}
                       </div>
                     </div>
-                    <div className="prose text-gray-500 max-w-none dark:text-gray-400">
-                      {summary}
+                    <div className="flex flex-wrap">
+                      {tags.map((tag) => (
+                        <Tag key={tag} text={tag} />
+                      ))}
                     </div>
                   </div>
                 </article>
