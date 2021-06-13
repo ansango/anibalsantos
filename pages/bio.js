d961eccd4edb6d6834a2523bfce53b4c527505ea
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-13 05:07:00 +0200

    fix bio

--- pages/bio.js
+++ pages/bio.js
@@ -11,7 +11,7 @@ export default function Bio() {
         description={`Bio - ${siteMetadata.author}`}
         url={`${siteMetadata.siteUrl}/bio`}
       />
-      <div className="divide-y">
+      <div className="pb-12">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Bio
