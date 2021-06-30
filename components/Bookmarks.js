2b39b6975eae5ac2d865fd047d696e62a5b39929
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-30 21:06:58 +0200

    sorted global fixed

--- components/Bookmarks.js
+++ components/Bookmarks.js
@@ -3,12 +3,11 @@ import { Disclosure, Transition } from '@headlessui/react'
 import Link from '@/components/Link'
 
 const Bookmarks = ({ posts }) => {
-  posts.sort(function (a, b) {
-    return new Date(a.date) - new Date(b.date)
-  })
-  const bookmarks = posts.filter(
-    (post) => post.tags.includes('bookmarks') && post.slug !== 'bookmarks/todos'
-  )
+  const bookmarks = posts
+    .filter((post) => post.tags.includes('bookmarks') && post.slug !== 'bookmarks/todos')
+    .sort(function (a, b) {
+      return new Date(a.date) - new Date(b.date)
+    })
   return (
     <Disclosure>
       {({ open }) => (
