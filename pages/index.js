53cb696225ff7a08a293dd8a400a8eaaed9fadcc
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-30 20:20:10 +0200

    accordion

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Bookmarks from '@/components/Bookmarks'
 import Link from '@/components/Link'
 import { PageSeo } from '@/components/SEO'
 import Tag from '@/components/Tag'
@@ -101,30 +102,8 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      <div>
-        <div className="py-10 space-y-2 md:space-y-5">
-          <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Bookmarks
-          </h2>
-        </div>
-        <ul className="pb-10 grid gap-3 md:grid-cols-2">
-          {bookmarks.map((bookmark) => {
-            const { slug, title, summary } = bookmark
-            return (
-              <li key={slug}>
-                <Link href={`/blog/${slug}`}>
-                  <article className="p-5 border border-gray-300 rounded-lg">
-                    <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-200">
-                      {title}
-                    </h3>
-                    <p className="prose text-gray-500 max-w-none dark:text-gray-400">{summary}</p>
-                  </article>
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+
+      <Bookmarks bookmarks={bookmarks} />
     </>
   )
 }
