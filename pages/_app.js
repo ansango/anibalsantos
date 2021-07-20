0a28144f9fce931ea2c092937e51ea7a3b9dccfd
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-20 17:21:21 +0200

    order

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,13 @@
 import '@/css/tailwind.css'
-
-import { ThemeProvider } from 'next-themes'
-import { DefaultSeo } from 'next-seo'
-import Head from 'next/head'
-
+import * as ga from '../lib/ga'
 import { SEO } from '@/components/SEO'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import ScrollToTop from '@/components/ScrollToTop'
-
+import { ThemeProvider } from 'next-themes'
+import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-
-import * as ga from '../lib/ga'
 
 export default function App({ Component, pageProps, router }) {
   const _router = useRouter()
