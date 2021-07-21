ba448ab48ad168d0703ff08abb7113e18d9bc748
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-21 21:20:15 +0200

    nav

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,45 +1,12 @@
-import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
-import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
-import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
+import Nav from './Nav'
 
 const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
       <div className="flex flex-col justify-between h-screen">
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label="Javascript Blog">
-              <div className="flex items-center justify-between">
-                <div className="w-2 h-2 p-2 mr-2 rounded-full bg-gradient-to-tr from-primary-200 to-primary-500"></div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="text-2xl font-semibold">{siteMetadata.headerTitle}</div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-            <ThemeSwitch />
-            <MobileNav />
-          </div>
-        </header>
+        <Nav />
         <main className="mb-auto">{children}</main>
         <Footer />
       </div>
---
+++ components/Nav.js
@@ -0,0 +1,122 @@
+import headerNavLinks from '@/data/headerNavLinks'
+import siteMetadata from '@/data/siteMetadata'
+import Link from './Link'
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+import {
+  HiSun as SunIcon,
+  HiMoon as MoonIcon,
+  HiMenu as MenuIcon,
+  HiX as XIcon,
+} from 'react-icons/hi'
+
+const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme, resolvedTheme } = useTheme()
+
+  useEffect(() => setMounted(true), [])
+
+  return (
+    <button
+      aria-label="Toggle Dark Mode"
+      type="button"
+      className="p-1 mx-1 rounded sm:ml-4"
+      onClick={() => setTheme(theme === 'dark' || resolvedTheme === 'dark' ? 'light' : 'dark')}
+    >
+      {mounted && (theme === 'dark' || resolvedTheme === 'dark') ? (
+        <SunIcon size={20} />
+      ) : (
+        <MoonIcon size={20} />
+      )}
+    </button>
+  )
+}
+
+const MobileNav = () => {
+  const [navShow, setNavShow] = useState(false)
+
+  const onToggleNav = () => {
+    setNavShow((status) => {
+      if (status) {
+        document.body.style.overflow = 'auto'
+      } else {
+        // Prevent scrolling
+        document.body.style.overflow = 'hidden'
+      }
+      return !status
+    })
+  }
+
+  return (
+    <div className="sm:hidden">
+      <button
+        type="button"
+        className="w-8 h-8 ml-1 mr-1 rounded"
+        aria-label="Toggle Menu"
+        onClick={onToggleNav}
+      >
+        {navShow ? <XIcon size={26} /> : <MenuIcon size={26} />}
+      </button>
+      <div
+        className={`fixed w-full h-full top-24 right-0 bg-gray-50 dark:bg-gray-900 opacity-95 z-10 transform ease-in-out duration-500 ${
+          navShow ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <button
+          type="button"
+          aria-label="toggle modal"
+          className="fixed w-full h-full cursor-auto focus:outline-none"
+          onClick={onToggleNav}
+        ></button>
+        <nav className="fixed h-full mt-8">
+          {headerNavLinks.map((link) => (
+            <div key={link.title} className="px-12 py-4">
+              <Link
+                href={link.href}
+                className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
+                onClick={onToggleNav}
+              >
+                {link.title}
+              </Link>
+            </div>
+          ))}
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+const Nav = () => {
+  return (
+    <div>
+      <header className="flex items-center justify-between py-10">
+        <Link href="/" aria-label="Javascript Blog">
+          <div className="flex items-center justify-between">
+            <div className="w-2 h-2 p-2 mr-2 rounded-full bg-gradient-to-tr from-primary-200 to-primary-500"></div>
+            {typeof siteMetadata.headerTitle === 'string' ? (
+              <div className="text-2xl font-semibold">{siteMetadata.headerTitle}</div>
+            ) : (
+              siteMetadata.headerTitle
+            )}
+          </div>
+        </Link>
+        <div className="flex items-center text-base leading-5">
+          <div className="hidden sm:block">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
+      </header>
+    </div>
+  )
+}
+export default Nav
