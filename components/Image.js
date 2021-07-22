d26b3d797f6d83bcd5d4675c851ab0e4d4dfaba1
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-22 16:50:35 +0200

    animation img

--- components/Image.js
+++ components/Image.js
@@ -1,5 +1,19 @@
 import NextImage from 'next/image'
+import { useState } from 'react'
 
-const Image = ({ ...rest }) => <NextImage {...rest} />
+const Image = ({ ...rest }) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const onLoadHandler = () => {
+    setIsLoaded(true)
+  }
+  return (
+    <div
+      className="opacity-0 transition-opacity duration-700"
+      style={{ opacity: isLoaded ? '100' : '0' }}
+    >
+      <NextImage {...rest} onLoad={onLoadHandler} />
+    </div>
+  )
+}
 
 export default Image
