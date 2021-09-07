060e5240f9f0a187c05a6b16d9c1ed82a1c22703
ansango <anibalsantosgo@gmail.com>
2021-09-07 12:36:07 +0200

    refactor dates

--- components/Contributions.js
+++ components/Contributions.js
@@ -7,22 +7,25 @@ const Contributions = () => {
   const { data } = useSWR('/api/contributions-current-year', Fetcher)
 
   return (
-    <>
+    <div className="flex flex-row mb-8 space-x-2 w-full">
+      <GithubIcon size={20} />
       {data?.contributionCalendar ? (
         <Link
-          className="text-gray-800 dark:text-gray-200 font-medium  max-w-max truncate flex flex-row mb-8 space-x-2 w-full"
+          className="text-gray-800 dark:text-gray-200 font-medium  max-w-max truncate"
           href="https://github.com/ansango"
         >
-          <GithubIcon size={20} />
           <span>Contribuciones en {new Date().getFullYear()}</span>
-          <span className="text-gray-500 dark:text-gray-300">
-            {` – ${data?.contributionCalendar.totalContributions}`}
-          </span>
         </Link>
       ) : (
-        <></>
+        <p className="text-gray-800 dark:text-gray-200 font-medium max-w-max truncate">
+          Sin registro
+        </p>
       )}
-    </>
+      <span className="mx-2 text-gray-500 dark:text-gray-300 block">{' – '}</span>
+      <p className="text-gray-500 dark:text-gray-300 max-w-max truncate">
+        {data?.contributionCalendar.totalContributions ?? 'Github'}
+      </p>
+    </div>
   )
 }
 
